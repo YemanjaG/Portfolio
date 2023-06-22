e6e78ac549ab1a1fa511f0b2d1f01e47ec39f674
Gabriel de Taxis <gabriel.detaxisdupoet@simpliciti.fr>
2023-06-22 11:59:22 +0200

    prog

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,17 +3,19 @@ import Home from './Components/Home';
 import Skills from './Components/Skills';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
+import About from './Components/About';
 import './CSS/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      <Skills />
-      <Projects />
-      <Contact />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Home/>
+            <About/>
+            <Skills/>
+            <Projects/>
+            <Contact/>
+        </div>
+    );
 }
 
 export default App;
